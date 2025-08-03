0bffa851ae569d2901489ef45fd136f73ee2f3d0
KimiPrime <kingsan049@gmail.com>
2025-08-03 19:51:32 +0700

    Add files via upload

--- IT102_CNTT6_SESSION6_bai9.c
+++ IT102_CNTT6_SESSION6_bai9.c
@@ -7,71 +7,71 @@ int main() {
 
     do {
         printf("\n\tMENU\n");
-        printf("1. Enter 3 numbers\n");
-        printf("2. Sum of 3 numbers\n");
-        printf("3. Average of 3 numbers\n");
-        printf("4. Smallest number\n");
-        printf("5. Largest number\n");
-        printf("6. Exit\n");
-        printf("Your choice: ");
+        printf("1. Nhập 3 số\n");
+        printf("2. Tổng 3 số\n");
+        printf("3. Trung bình cộng 3 số\n");
+        printf("4. Số nhỏ nhất\n");
+        printf("5. Số lớn nhất\n");
+        printf("6. Thoát\n");
+        printf("Lựa chọn của bạn: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                printf("Enter first number: ");
+                printf("Nhập số thứ nhất: ");
                 scanf("%d", &num1);
-                printf("Enter second number: ");
+                printf("Nhập số thứ hai: ");
                 scanf("%d", &num2);
-                printf("Enter third number: ");
+                printf("Nhập số thứ ba: ");
                 scanf("%d", &num3);
                 isEntered = 1;
                 break;
 
             case 2:
                 if (!isEntered) {
-                    printf("You haven't entered 3 numbers yet!\n");
+                    printf("Bạn chưa nhập 3 số!\n");
                 } else {
-                    printf("Sum: %d\n", num1 + num2 + num3);
+                    printf("Tổng: %d\n", num1 + num2 + num3);
                 }
                 break;
 
             case 3:
                 if (!isEntered) {
-                    printf("You haven't entered 3 numbers yet!\n");
+                    printf("Bạn chưa nhập 3 số!\n");
                 } else {
                     float average = (num1 + num2 + num3) / 3.0;
-                    printf("Average: %.2f\n", average);
+                    printf("Trung bình cộng: %.2f\n", average);
                 }
                 break;
 
             case 4:
                 if (!isEntered) {
-                    printf("You haven't entered 3 numbers yet!\n");
+                    printf("Bạn chưa nhập 3 số!\n");
                 } else {
                     int min = num1;
                     if (num2 < min) min = num2;
                     if (num3 < min) min = num3;
-                    printf("Smallest number: %d\n", min);
+                    printf("Số nhỏ nhất: %d\n", min);
                 }
                 break;
 
             case 5:
                 if (!isEntered) {
-                    printf("You haven't entered 3 numbers yet!\n");
+                    printf("Bạn chưa nhập 3 số!\n");
                 } else {
                     int max = num1;
                     if (num2 > max) max = num2;
                     if (num3 > max) max = num3;
-                    printf("Largest number: %d\n", max);
+                    printf("Số lớn nhất: %d\n", max);
                 }
                 break;
 
             case 6:
-                printf("Exiting program...\n");
+                printf("Đang thoát chương trình...\n");
                 break;
 
             default:
-                printf("Invalid choice. Please try again.\n");
+                printf("Lựa chọn không hợp lệ. Vui lòng chọn lại.\n");
         }
 
     } while (choice != 6);
